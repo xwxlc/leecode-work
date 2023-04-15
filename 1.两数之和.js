4dcd7be14a7b40ed39955c0d2a0eadeb84503b62
夏玮 <1006903445@qq.com>
2023-04-15 10:59:23 +0800

    1 9

---
+++ 1.两数之和.js
@@ -0,0 +1,27 @@
+// https://leetcode.cn/problems/two-sum/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const paramNums = [2, 7, 11, 15];
+const paramTarget = 9;
+/**
+ * 解题思路
+ * 先创建一个map记录 差值和当前下标的关联表 key:target-nums[i] value:i
+ * 后续的元素只要map中寻找到对应的记录，说明两数匹配
+ */
+
+var twoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      return [map.get(nums[i]), i];
+    } else {
+      map.set(target - nums[i], i);
+    }
+  }
+};
+
+console.log(twoSum(paramNums, paramTarget));
---
+++ 9.回文数.js
@@ -0,0 +1,40 @@
+// https://leetcode.cn/problems/palindrome-number/
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const argv = process.argv;
+const paramX = argv[2] ? Number(argv[2]) : 121;
+
+/**
+ * 解题思路
+ * 首先判断 x<0 时 直接返回false
+ * 然后 转为字符串判断
+ */
+var isPalindrome = function (x) {
+  if (x < 0 || (x % 10 == 0 && x != 0)) {
+    return false;
+  }
+  const xArr = x.toString().split('');
+  return xArr.toString() == xArr.reverse().toString();
+};
+console.log(isPalindrome(paramX));
+
+/**
+ * 解题思路
+ * 首先判断 x<0 时 直接返回false
+ * 然后 计算出一个新数字
+ */
+var isPalindrome2 = function (x) {
+  if (x < 0 || (x % 10 == 0 && x != 0)) {
+    return false;
+  }
+  let reverseNumber = 0;
+  while (reverseNumber < x) {
+    reverseNumber = reverseNumber * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+  return x == reverseNumber || x == Math.floor(reverseNumber / 10);
+};
+console.log(isPalindrome2(paramX));
